d6c2d81763d53856bc849147e38b8179da39d92d
Josef Biehler <jbiehler@samhammer.de>
2018-11-10 23:17:01 +0100

    first commit: move code from ib-flex-reader to own project

---
+++ EnumParser/EnumParser/EnumNameAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EnumParser
+{
+    public class EnumNameAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public EnumNameAttribute(string value = null)
+        {
+            Value = value;
+        }
+    }
+}
